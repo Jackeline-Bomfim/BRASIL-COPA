4dd9a3f21935ed57bae122cbe26e6ccd0fc158c4
Jackeline-Bomfim <kelinehta12@gmail.com>
2022-11-27 22:46:07 -0300

    subindo o repositorio

---
+++ src/app/datas/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Richarlison entra no top 10 de artilheiros da Seleção Brasileira no século",
+        "description": "Destaque do Brasil na estreia da Copa do Mundo, Richarlison atingiu um número importante com a amarelinha ao marcar os dois gols da vitória por 2 a 0 sobre a Sérvia, na quinta-feira (24/11). Aos 25 anos, o atacante entrou para o ranking dos 10 maiores artilheiros da Seleção no século XXI, com 19 bolas nas redes.",
+        "photoCover": "https://i.superesportes.com.br/X-x3aO2mYRufZLYGfvBspeSXWXA=/650x0/smart/imgsapp.mg.superesportes.com.br/app/noticia_126420360808/2022/11/27/3981396/neymar-ronaldo-ronaldinho-e-richarlison-em-jogos-pela-selecao-brasileira_1_47936.jpg"
+    },
+    {
+        "id": "2",
+        "title": "O que o Brasil precisa para se classificar já nesta segunda-feira (28)",
+        "description": "Com a vitória na estreia, contra a Sérvia, a Seleção Brasileira somou 3 pontos no grupo G da Copa do Mundo no Catar, que ainda conta com Suíça e Camarões. O resultado dá ao Brasil a chance de se classificar para as oitavas já na segunda rodada da primeira fase, quando enfrenta os suíços nesta segunda-feira (28). Para conseguir a classificação antecipada na Copa do Mundo, o Brasil precisa vencer a Suíça por qualquer placar e torcer ou para Sérvia e Camarões empatarem ou para os sérvios vencerem os camaroneses.",
+        "photoCover": "https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2022/11/52521733685_6d366c0ae4_k-e1669323087445.jpg?w=876&h=484&crop=1"
+    },
+    {
+        "id": "3",
+        "title": "Virose afeta jogadores da seleção brasileira e preocupa Tite",
+        "description": "Catar - Além das lesões de Danilo e Neymar, o técnico Tite tem outros problemas com a delegação da seleção brasileira na Copa do Mundo do Catar. Isto porque, uma forte virose tem afetado alguns jogadores, deixando uma preocupação na comissão técnica e no departamento médico da Seleção. A informação é do portal GOAL.",
+        "photoCover": "https://odia.ig.com.br/_midias/jpg/2022/11/27/1200x750/1_52527111859_abeae39f80_k-27212604.jpg"
+    },
+    {
+        "id": "4",
+        "title": "Logo na Copa? Neymar volta a sofrer lesão após um ano",
+        "description": "Quis o destino que a primeira lesão sofrida por Neymar nesta temporada — o primeiro problema físico mais sério em um ano — acontecesse justamente na Copa do Mundo 2022, durante a estreia empolgante da seleção brasileira na vitória sobre a Sérvia.Conforme informado pela CBF, os exames médicos realizados nesta sexta-feira apontaram uma lesão nos ligamentos do tornozelo direito, com um edema ósseo.",
+        "photoCover": "https://s2.glbimg.com/5vB1xUgU9lXPvY3xlwSRpK-fz2A=/0x0:1920x1056/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2022/0/A/UNZB4xSY6mgB9pIY04xA/14324f83be03f89778c532b1db971f2e3d6866a8w.jpg"
+    }
+]
